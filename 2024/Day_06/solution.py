791e44ba7343d30ad2f94cd71fc234b4b03a2759
bdefnet-cedar <125094942+bdefnet-cedar@users.noreply.github.com>
2024-12-05 23:12:40 -0800

    Cleaned up code and added comments

--- 2024/Day_06/solution.py
+++ 2024/Day_06/solution.py
@@ -27,6 +27,7 @@ def traverse_map(my_map, trail, check_for_loop):
     while True:
         my_map[guard_pos[0]][guard_pos[1]] = 'X'
 
+        # we have a loop if we've been here before traveling in the same direction
         if check_for_loop and tuple(guard_pos) in trail and dir in trail[tuple(guard_pos)]:
             #my_map = '\n'.join(''.join(row) for row in my_map)
             #print(my_map)
@@ -61,20 +62,28 @@ with open('input.txt', 'r') as f:
 
     trail = {}
 
-    traverse_map(my_map, trail, False)
+    #Part 1 - traverse map and count X's 
+    #Store trail of positions traversed and in which directions
+    traverse_map(my_map, trail, check_for_loop=False)
     count = sum(row.count('X') for row in my_map)
     
+    #Part 2
+    #Traverse map again, but this time check for loops  
     loop_count = 0
     first = True
+    #for each position in the original trail, insert an obstacle and check for loops
     for idx, pos in enumerate(trail.keys()):
         print(f"Index: {idx}")
         if first:
+            #skip first position since that is where the guard starts
             first = False
             continue
+        #reset map to original state
         my_map = [[char for char in row] for row in original_map]
+        #insert obstacle
         my_map[pos[0]][pos[1]] = '#'
         new_trail = {}
-        if traverse_map(my_map, new_trail, True):
+        if traverse_map(my_map, new_trail, check_for_loop=True):
             loop_count += 1
 
     print(f"Number of X's: {count}")
